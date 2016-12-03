3368ee4b803f7d850157ac7630e387ac99ee2007
Dylan <djhahn@ncsu.edu>
2016-12-03 18:14:21 -0500

    automated entity adding

--- WorkingTitle/mainFlow.js
+++ WorkingTitle/mainFlow.js
@@ -1,5 +1,6 @@
 /* GLOBAL CONSTANTS AND VARIABLES */
 var GAME = {};
+var BASE_URL = "https://raw.githubusercontent.com/ForgeDH/csc-graphics-game/master/WorkingTitle/";
 
 GAME.allObjects = [];			// everything in the world
 GAME.entities = [];				// things that need processing every tick (player, enemies, projectiles, etc.)
@@ -7,6 +8,16 @@ GAME.structures = [];			// things that are static (walls, floors, etc.)
 
 GAME.player;
 
+
+
+/* ARENA FILE STRUCTURE */
+var Arena1 = ["floor1.json"];
+var ARENAS = {"Arena1":Arena1};
+
+/* ENTITY FILE STRUCTURE */
+var OBJECTS = ["object1.json"]
+var ENEMIES = [];
+var ENTITIES = {"Enemies":ENEMIES, "Objects":OBJECTS};
 
 /* MAIN -- HERE is where execution begins after window load */
 
@@ -30,8 +41,26 @@ function main() {
 	GAME.world.broadphase = new CANNON.NaiveBroadphase();
 
 	// add entities manually
-	GAME.entities.push(new Entity('https://raw.githubusercontent.com/ForgeDH/csc-graphics-game/master/WorkingTitle/Arenas/Arena1/Floors/floor1.json'));
-	GAME.entities.push(new Entity('https://raw.githubusercontent.com/ForgeDH/csc-graphics-game/master/WorkingTitle/Entities/Objects/Object1/Object1.json'));
+	//GAME.entities.push(new Entity(BASE_URL + 'Arenas/Arena1/Floors/floor1.json'));
+	//GAME.entities.push(new Entity(BASE_URL + 'Entities/Objects/Object1/Object1.json'));
+	
+	// add entities (all for now)
+	for (var arena in ARENAS){
+		for (var obj in ARENAS[arena]){
+			console.log(BASE_URL + 'Arenas/' + arena + '/' + ARENAS[arena][obj]);
+			GAME.entities.push(new Entity(BASE_URL + 'Arenas/' + arena + '/' + ARENAS[arena][obj]));
+		}
+	}
+	for (var entity in ENTITIES){
+		for (var obj in OBJECTS){
+			console.log(BASE_URL + 'Entities/Objects/' + obj + '/' + ENTITIES[entity][obj]);
+			GAME.entities.push(new Entity(BASE_URL + 'Entities/Objects/' + obj + '/' + ENTITIES[entity][obj]));
+		}
+		for (var enemy in ENEMIES){
+			console.log(BASE_URL + 'Entities/Enemies/' + enemy + '/' + ENTITIES[entity][enemy]);
+			GAME.entities.push(new Entity(BASE_URL + 'Entities/Enemies/' + enemy + '/' + ENTITIES[entity][enemy]));
+		}
+	}
 	console.log(GAME.entities);
 	
 	GAME.camera.position.y = 5;
