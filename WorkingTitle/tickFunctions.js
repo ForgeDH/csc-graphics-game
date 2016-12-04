0750cb7f0e1812607db5b33f8d4e4235c963ad27
Dylan <djhahn@ncsu.edu>
2016-12-03 22:22:35 -0500

    motion and camera direction

--- WorkingTitle/tickFunctions.js
+++ WorkingTitle/tickFunctions.js
@@ -6,17 +6,33 @@ tickFunctions.noTick = function(){
 tickFunctions.boxTick = function(){
 	// POSITION
 	var ms = 0.5;
+	var speedCap = 1;
+	var upVec = new CANNON.Vec3(0,1,0);
+	
+	var facingAngle = this.mesh.yawObj.rotation.y;
+	var forwardVec = new CANNON.Vec3(-Math.sin(facingAngle), 0, -Math.cos(facingAngle));
+	forwardVec.normalize();
+
+	var rightVec = new CANNON.Vec3();
+	forwardVec.cross(upVec, rightVec);
+	rightVec.normalize();
+	
 	if(INPUT.isKeyDown("w")){
-		this.body.velocity.z -= ms;
+		this.body.velocity.vadd(forwardVec.scale(ms, forwardVec), this.body.velocity);
 	}
 	if(INPUT.isKeyDown("s")){
-		this.body.velocity.z += ms;
+		this.body.velocity.vsub(forwardVec.scale(ms, forwardVec), this.body.velocity);
 	}
 	if(INPUT.isKeyDown("a")){
-		this.body.velocity.x -= ms;
+		this.body.velocity.vsub(rightVec.scale(ms, rightVec), this.body.velocity);
 	}
 	if(INPUT.isKeyDown("d")){
-		this.body.velocity.x += ms;
+		this.body.velocity.vadd(rightVec.scale(ms, rightVec), this.body.velocity);
+	}
+	
+	if(this.body.velocity.norm() > speedCap){
+		this.body.velocity.normalize();
+		this.body.velocity.scale(speedCap, this.body.velocity);
 	}
 	
 	//GAME.camera.position.x = this.body.position.x;
