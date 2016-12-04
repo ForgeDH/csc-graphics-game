3403325d3fd9f0d0c4417c9c60908567199193aa
Dylan <djhahn@ncsu.edu>
2016-12-03 23:33:43 -0500

    scene groundwork

--- WorkingTitle/mainFlow.js
+++ WorkingTitle/mainFlow.js
@@ -1,5 +1,6 @@
 /* GLOBAL CONSTANTS AND VARIABLES */
 var GAME = {};
+var activeScene;
 var BASE_URL = "https://raw.githubusercontent.com/ForgeDH/csc-graphics-game/master/WorkingTitle/";
 
 GAME.allObjects = [];			// everything in the world
@@ -7,8 +8,6 @@ GAME.entities = [];				// things that need processing every tick (player, enemie
 GAME.structures = [];			// things that are static (walls, floors, etc.)
 
 GAME.player;
-
-
 
 /* ARENA FILE STRUCTURE */
 var Arena1 = ["floor1.json"];
@@ -21,60 +20,12 @@ var ENEMIES = {};
 /* MAIN -- HERE is where execution begins after window load */
 
 function main() {
-  
-	// create THREE scene, light and camera
-	GAME.scene = new THREE.Scene();
-	var light = new THREE.PointLight( 0xffffff, 1, 10000 );
-	light.position.set( 50, 50, 50 );
-	GAME.scene.add( light );
-	GAME.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-	// create THREE renderer and put it in the webpage
-	GAME.renderer = new THREE.WebGLRenderer();
-	GAME.renderer.setSize( window.innerWidth * 0.9, window.innerHeight * 0.9 );
-	document.body.appendChild( GAME.renderer.domElement );
-
-	// create CANNON world
-	GAME.world = new CANNON.World();
-	GAME.world.gravity.set(0,-1,0);
-	GAME.world.broadphase = new CANNON.NaiveBroadphase();
-	
-	// add entities (all for now)
-	for (var arena in ARENAS){
-		for (var obj in ARENAS[arena]){
-			// TODO SWITCH OFF OF ENTITIES
-			GAME.entities.push(new Entity(BASE_URL + 'Arenas/' + arena + '/' + ARENAS[arena][obj]));
-		}
-	}
-	for (var obj in OBJECTS){
-		GAME.entities.push(new Entity(BASE_URL + 'Entities/Objects/' + obj + '/' + OBJECTS[obj]));
-	}
-	for (var enemy in ENEMIES){
-		GAME.entities.push(new Entity(BASE_URL + 'Entities/Enemies/' + enemy + '/' + ENEMIES[enemy]));
-	}
-	console.log(GAME.entities);
-	
-	// load player
-	GAME.player = new Entity(BASE_URL + 'Entities/Objects/object1/object1.json', true);
-	GAME.entities.push(GAME.player);
-	//Entity.initPlayer();
+	activeScene = new Scene(gameSceneInit, gameSceneLoop);
 	
 	function render() {
 		requestAnimationFrame( render );
 		
-		//handle input
-		while (INPUT.eventsToHandle() == true){
-			//console.log(INPUT.getNextEvent());
-			INPUT.getNextEvent();
-		}
-		
-		for (var idx in GAME.entities){
-			GAME.entities[idx].tick();
-			GAME.entities[idx].updateMeshToBody();
-		}
-		
-		GAME.world.step(0.1666);
-		GAME.renderer.render( GAME.scene, GAME.camera );
+		activeScene.runningLoop();
 	}
 	
 	INPUT.init();
