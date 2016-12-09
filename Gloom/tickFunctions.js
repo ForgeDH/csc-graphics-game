974eee71e6acd4dd9d34737d920e78bc6e101178
Riounis <sjrappl@ncsu.edu>
2016-12-09 02:42:31 -0500

    Update tickFunctions.js

--- Gloom/tickFunctions.js
+++ Gloom/tickFunctions.js
@@ -15,7 +15,7 @@ var killObject = function(deadObj){
 		GAME.world.remove(deadObj.body);
 		GAME.scene.remove(deadObj.mesh.parent);
 		
-		if(Math.random() > 0.5) {
+		if(Math.random() > 0.01) {
 		  addEntity(new HPack(deadObj));
 		}
 	}
