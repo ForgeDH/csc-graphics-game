329047069a52aa01d61b91bafce60937c095aa7e
jbinkrot <jbinkrot@ncsu.edu>
2016-12-08 22:06:37 -0500

    Macfuckery

--- WorkingTitle/mainFlow.js
+++ WorkingTitle/mainFlow.js
@@ -188,7 +188,7 @@ function load(JSONurl, name) {
 			    if(resources[name+"texture"]){
 				    child.material.map = resources[name+"texture"];
 			    } else {
-				    child.material = new THREE.MeshPhongMaterial({color: 0xdddddd, specular: 0xdddddd, shininess: 30, shading: THREE.FlatShading});;
+				    child.material = new THREE.MeshPhongMaterial({color: 0xdddddd, specular: 0xdddddd, shininess: 30, shading: THREE.FlatShading, side: THREE.DoubleSide});;
 			    }
 			    resources[name+"mesh"] = child;
 		    }
